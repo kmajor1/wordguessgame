af93480f59953cd2f0e82556bc04cc8e589457d4
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-03 01:05:47 -0400

    created basic variables in JS file. Basic container in index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// define the alphabet 
+var alphabet = ["A", "B", "C", "D", "E", "F",
+"G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+"S", "T", "U", "V", "W", "X", "Y", "Z", "-", " ", "!"];
+
+var lettersGuessed = [];
+
+// define different words from the theme Jurassic Park
+var WordsToGuess = ["Chromosones", "Velociraptor", "Unix", "Shoot Her!", 
+"Isla Nubla", "InGen", "Animatronics", ""]
+
+// construct "game" object 
+// game started property (boolean)
+// function initGame --> initializes game. Based on any key being pressed. 
+    // randomly select a word to have the user guess
+    // measure the length of the word 
+    // create necessary number of boxes, uses HTML elements to display the word, based on that word length
+    // ensure the contents of the box are hidden (using CSS selector)
+    // 
+
+// guesses remaining property
+// letters guessed property
+// function guess letter
+    // add letter guessed to the lettersGuessed array 
+    // modify somehow the grid of 'guessed' letters to indicate that the letter has been used. 
+    
