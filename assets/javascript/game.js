933312b59da703ba843c7dc5ad1364acff618070
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-08 22:42:28 -0400

    updated basic framework of game object. Added keyup event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
 // define the alphabet 
+ window.onload = function mycode (e) {
+
+
 var alphabet = ["A", "B", "C", "D", "E", "F",
 "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
 "S", "T", "U", "V", "W", "X", "Y", "Z", "-", " ", "!"];
@@ -10,33 +13,53 @@ var jurassicWords = ["Chromosones", "Velociraptor", "Unix", "Shoot Her!",
 "Isla Nubla", "InGen", "Animatronics", "Hammond"];
 
 var jurassicWords2 = ["testing", "a theory"];
+var wordLen = 0;
 
 
 
 // game object
 function wordGame(words, lettersGuessed, GuessesRemaining, gameStarted) {
-   
     this.lettersGuessed = lettersGuessed;
     this.GuessesRemaining = GuessesRemaining; 
     this.words = words; 
+    /* 
+    The function selectWord accesses the words passed to the object when instantiated
+    It determines length of the array, and selects a random index. 
+    */
     this.selectWord = function () {
-        var arrayLength = this.words.length;
-       // after getting length of array, selected a random number within that length 
-       // comment out line 19 once done debugging. 
-       console.log(Math.floor(Math.random() * (arrayLength  + 1)));
-       this.selectedWord = this.words[Math.floor(Math.random() * (arrayLength  + 1))]; 
+        // check array length
+        var arrayLength = this.words.length; 
+        // select random index from words array, assign to selectedWord property of the object 
+        this.selectedWord = this.words[Math.floor(Math.random() * (arrayLength  + 1))]; 
+        // determine the length of the word of this object, assign to global var wordLen 
+        wordLen = this.selectedWord.length; 
     }
+    // default value of gameStarted property of object is false 
     this.gameStarted = false; 
+    // method that can be invoked on object to start game 
     this.startGame = function () {
         this.gameStarted = true; 
     }
     this.createLetterContainers = function () { 
-        // check if word selected is non-null
-        //determine the length of word selected 
-        //create child divs in the proper container 
+        // check if word selected is greater than 0 
+        // create child containers in main container div   
+        
+        // create child divs in the proper container 
+        // assign a data-* attribute to each letter container, indicating what letter it is
+        // technically, user could view source to 'cheat'. But this is unlikely and easier 
+        // than the alternative code required. 
     }
-    this.checkLetter = function () {
-        // read the 
+    this.revealLetter = function (userLetter) {
+        // read the input of the user, check if there are matches within the word 
+        // read the content of each data-* attribute and stick in array 
+        // cycle through each data-* attribute. 
+        for (var i = 0; i<this.selectedWord.length; i++) {
+            var letterCheck = this.selectedWord[i];
+            if (letterCheck == userLetter) {
+                // find the divs with the matching data attribute, reveal them.  
+            }
+        }
+        console.log(this.selectedWord); 
     }
 }
 
@@ -45,18 +68,28 @@ function wordGame(words, lettersGuessed, GuessesRemaining, gameStarted) {
 
 var Game = new wordGame(jurassicWords, lettersGuessed, 10);
 
-document.onkeyup = function () {
-    alert("test");
-    if (!Game.gameStarted) {
-        // start the game 
+document.onkeyup = function (event) {
+    if (!Game.gameStarted) { 
         Game.startGame();
+        console.log("Game Started")
     }
     else {
-        // call the key checking functions 
+        if (event.key.toUpperCase =='A') {
+            console.log("Key A Pressed");
+        }
     }
     
     
 }
+
+
+//test createdwordfunction scope 
+Game.selectWord(); 
+console.log(wordLen);
+console.log(Game.selectedWord)
+Game.checkLetter(); 
+ }
+
 // instantiate the object 
  
 
